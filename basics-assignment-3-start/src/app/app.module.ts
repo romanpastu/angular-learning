3412b4c3868538b7260133578adf8f829edcbe41
romanpastu <roman.pastushenko.1997@gmail.com>
2019-03-21 10:52:16 +0100

    update

--- basics-assignment-3-start/src/app/app.module.ts
+++ basics-assignment-3-start/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DisplayComponent } from './display/display.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayComponent
+
   ],
   imports: [
     BrowserModule,
---
+++ basics-assignment-3-start/src/app/display/display.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display',
+  templateUrl: './display.component.html',
+  styles: [`
+  .plus5 {
+      color: white;
+  }
+`]
+})
+export class DisplayComponent implements OnInit {
+  passCreated = false;
+  nOfDisplays = 0;
+  constructor() { }
+  displays :number[] = [];
+  ngOnInit() {
+  }
+
+  onShowDisplay(){
+    this.nOfDisplays++;
+    this.displays.push(+ new Date());
+    if(this.passCreated == true){
+      this.passCreated = false;
+    }else{
+      this.passCreated = true;
+    }
+    console.log(this.getColor());
+  }
+
+  getColor(){
+    return this.displays.length >= 5 ? "blue" : "white" ;
+    
+  }
+  
+
+}
